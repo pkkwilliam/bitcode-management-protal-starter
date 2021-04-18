5890ace194bc23c72615d4812abd5a454ebc13d8
pkkwilliam <pkkwilliam919@gmail.com>
2021-04-18 21:13:39 +0800

    test ant table

--- src/component/companyManagement/categoryManagement/CategoryManagement.view.js
+++ src/component/companyManagement/categoryManagement/CategoryManagement.view.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ApplicationComponentView from "src/common/ApplicationComponentView";
 import {
   CBadge,
@@ -6,9 +6,14 @@ import {
   CCardBody,
   CCardHeader,
   CCol,
+  CCollapse,
   CDataTable,
   CRow,
 } from "@coreui/react";
+
+export const ID_COLUMN = { key: "id", label: "ID" };
+export const ITEM_DETAIL_COLUMN = { key: "details", label: "" };
+export const NAME_COLUMN = { key: "name", label: "類別" };
 
 export default function CategoryManagementView(props) {
   return (
@@ -21,8 +26,23 @@ export default function CategoryManagementView(props) {
 }
 
 export function CategoryTable(props) {
+  const [showDetailId, setShowDetailId] = useState(0);
   const { categories } = props;
-  return <CDataTable items={categories} />;
+  return (
+    <CDataTable
+      items={categories}
+      fields={[ID_COLUMN, NAME_COLUMN, ITEM_DETAIL_COLUMN]}
+      scopedSlots={{
+        details: (category, index) => {
+          return (
+            <CCollapse show={showDetailId === category.id}>
+              <CCardBody></CCardBody>
+            </CCollapse>
+          );
+        },
+      }}
+    />
+  );
 }
 
 export function ModifyButtonRow(props) {
