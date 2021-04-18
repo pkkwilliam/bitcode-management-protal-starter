f48a189a4bd1dbf3fe1ef9439e4295241f9a7d46
pkkwilliam <pkkwilliam919@gmail.com>
2021-04-18 15:21:42 +0800

    test anyd table

--- src/component/companyManagement/categoryManagement/CategoryManagement.js
+++ src/component/companyManagement/categoryManagement/CategoryManagement.js
@@ -1,13 +1,27 @@
 import React from "react";
 import ApplicationComponent from "src/common/ApplicationComponent";
+import { GET_CATEGORIES } from "src/service/service";
 import CategoryManagementView from "./CategoryManagement.view";
 
 export default class CategoryManagement extends ApplicationComponent {
   state = {
     ...this.state,
+    categories: [],
   };
 
+  componentDidMount() {
+    super.componentDidMount();
+    this.getCategories();
+  }
+
   render() {
+    console.log(this.state.categories);
     return <CategoryManagementView {...this.state} />;
+  }
+
+  getCategories() {
+    this.serviceExecutor
+      .execute(GET_CATEGORIES())
+      .then((categories) => this.setState({ categories }));
   }
 }
--- src/component/companyManagement/categoryManagement/CategoryManagement.view.js
+++ src/component/companyManagement/categoryManagement/CategoryManagement.view.js
@@ -1,10 +1,30 @@
 import React from "react";
 import ApplicationComponentView from "src/common/ApplicationComponentView";
+import {
+  CBadge,
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CDataTable,
+  CRow,
+} from "@coreui/react";
 
 export default function CategoryManagementView(props) {
   return (
     <ApplicationComponentView {...props}>
-      CategoryManagementView
+      <CCard>
+        <CategoryTable {...props} />
+      </CCard>
     </ApplicationComponentView>
   );
+}
+
+export function CategoryTable(props) {
+  const { categories } = props;
+  return <CDataTable items={categories} />;
+}
+
+export function ModifyButtonRow(props) {
+  const { onClickAdd, onClickDelete, onClickEdit, onClickUpdate } = props;
 }
